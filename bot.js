59e82fbdafa3fff3752a65f3178eadb75d43997c
OmarTamer55 <45062049+OmarTamer55@users.noreply.github.com>
2018-11-19 23:00:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,23 @@ client.on('ready', () => {
 
 
 
-// CODES HERE
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
+});
+
+
+client.on('ready', async() => {
+var server = "Id server"; // 512532251850833934
+var channel = "id room";//514182082855960576
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam ,  , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , ')
+    },400);
+})
+
+client.login(NTEzNDg4ODQ2MDc2MjQ4MDY0.DtS1TQ.qXC1Y1p0hI3N6239BAnsYsLkL-o);
 
 
 
